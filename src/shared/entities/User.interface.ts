9c6ebb8cf9977bc88ed59374cb7833a6faf94714
es0230 <caneyok@gmail.com>
2024-05-05 14:12:53 +0400

    fixes from last pr

--- src/shared/entities/User.interface.ts
+++ src/shared/entities/User.interface.ts
@@ -1,7 +1,9 @@
+import { UserStatus } from '../types/index.js';
+
 export interface User {
   name: string;
   email: string;
   avatar?: string; // изображение
   password: string;
-  status: 'common' | 'pro';
+  status: UserStatus;
 }
